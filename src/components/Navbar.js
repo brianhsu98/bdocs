120ba260f752ac653db73783083ed44cdc09b96f
Brian Hsu <brianhsu@berkeley.edu>
2018-12-28 00:35:41 -0800

    [Deployment] Adding gh-pages dependency and setting up package.lock

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -12,13 +12,16 @@ class NavBar extends React.Component {
         <div>
           <Switch>
             <Menu borderless pointing>
-              <Menu.Item as={NavLink} exact to="/">
+              <Menu.Item as={NavLink} exact to={process.env.PUBLIC_URL + "/"}>
                 Home
               </Menu.Item>
             </Menu>
           </Switch>
-          <Route exact path="/" component={Home} />
-          <Route path="/editor/:id" component={Editor} />
+          <Route exact path={process.env.PUBLIC_URL + "/"} component={Home} />
+          <Route
+            path={process.env.PUBLIC_URL + "/editor/:id"}
+            component={Editor}
+          />
         </div>
       </BrowserRouter>
     );
