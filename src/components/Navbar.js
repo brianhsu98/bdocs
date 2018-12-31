fa0663839d10e3e6601a95fb13817091f70543d2
Brian Hsu <brianhsu@berkeley.edu>
2018-12-30 17:50:19 -0800

    [Feature] Cookie support: Passing on cookies as props to relevant components

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -4,6 +4,7 @@ import { Menu } from "semantic-ui-react";
 import { Route, BrowserRouter, Switch } from "react-router-dom";
 import Home from "./Home";
 import Editor from "./Editor";
+import { withCookies } from "react-cookie";
 
 class NavBar extends React.Component {
   render() {
@@ -17,14 +18,22 @@ class NavBar extends React.Component {
               </Menu.Item>
             </Menu>
           </Switch>
-          <Route exact path={"/"} component={Home} />
+          <Route
+            exact
+            path={"/"}
+            render={props => <Home {...props} cookies={this.props.cookies} />}
+          />
           <Route
             path={"/editor/:id"}
-            render={props => <Editor {...props} isCode={false} />}
+            render={props => (
+              <Editor {...props} isCode={false} cookies={this.props.cookies} />
+            )}
           />
           <Route
             path={"/code/:id"}
-            render={props => <Editor {...props} isCode={true} />}
+            render={props => (
+              <Editor {...props} isCode={true} cookies={this.props.cookies} />
+            )}
           />
         </div>
       </BrowserRouter>
@@ -32,4 +41,4 @@ class NavBar extends React.Component {
   }
 }
 
-export default NavBar;
+export default withCookies(NavBar);
--- src/index.js
+++ src/index.js
@@ -1,6 +1,12 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { CookiesProvider } from "react-cookie";
 import "./index.css";
 import App from "./App";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <CookiesProvider>
+    <App />
+  </CookiesProvider>,
+  document.getElementById("root")
+);
